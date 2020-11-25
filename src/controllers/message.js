3485f845ce3623f2d32b486766426e1a84dfb2ce
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-25 14:44:59 +0700

    add pagination on get message list & message detail

--- src/controllers/message.js
+++ src/controllers/message.js
@@ -1,7 +1,9 @@
 const responseStandard = require('../helpers/responses')
 const Joi = require('joi')
 const { Op } = require('sequelize')
+const qs = require('querystring')
 
+const { APP_PORT, BASE_URL } = process.env
 const { Messages, Users, sequelize } = require('../models')
 
 module.exports = {
@@ -60,6 +62,30 @@ module.exports = {
   getMessageDetail: async (req, res) => {
     const { id } = req.user
     const { friendId } = req.params
+    let { page } = req.query
+    const limit = 20
+
+    if (!page) {
+      page = 1
+    } else {
+      const schema = Joi.object({
+        page: Joi.number().integer().min(1)
+      })
+      const { error, value } = schema.validate({ page: page })
+      if (error) {
+        return responseStandard(res, error.message, {}, 400, false)
+      }
+      page = value.page
+    }
+
+    const pageInfo = {
+      count: 0,
+      pages: 0,
+      currentPage: page,
+      limitPerPage: limit,
+      nextLink: null,
+      prevLink: null
+    }
 
     const message = await Messages.findAll({
       where: {
@@ -88,12 +114,52 @@ module.exports = {
       },
       order: [
         ['createdAt', 'DESC']
-      ]
+      ],
+      limit: limit,
+      offset: (page - 1) * limit
     })
 
     if (message.length) {
+      const count = await Messages.count({
+        where: {
+          [Op.and]: [
+            {
+              [Op.or]: [
+                {
+                  sender_id: id
+                },
+                {
+                  receiver_id: id
+                }
+              ]
+            },
+            {
+              [Op.or]: [
+                {
+                  sender_id: friendId
+                },
+                {
+                  receiver_id: friendId
+                }
+              ]
+            }
+          ]
+        }
+      })
+      pageInfo.count = count
+      pageInfo.pages = Math.ceil(count / limit)
+      const { pages, currentPage } = pageInfo
+
+      if (currentPage < pages) {
+        pageInfo.nextLink = `${BASE_URL}:${APP_PORT}/message/${friendId}?${qs.stringify({ ...req.query, ...{ page: page + 1 } })}`
+      }
+
+      if (currentPage > 1) {
+        pageInfo.prevLink = `${BASE_URL}:${APP_PORT}/message/${friendId}?${qs.stringify({ ...req.query, ...{ page: page - 1 } })}`
+      }
+
       const user = await Users.findByPk(friendId, { attributes: ['id', 'name'] })
-      return responseStandard(res, 'Found message detail!', { result: { friend: user, message } })
+      return responseStandard(res, 'Found message detail!', { pageInfo, result: { friend: user, message } })
     } else {
       return responseStandard(res, 'Message not found!', {}, 404, false)
     }
@@ -137,6 +203,30 @@ module.exports = {
   },
   getMessageList: async (req, res) => {
     const { id } = req.user
+    let { page } = req.query
+    const limit = 20
+
+    if (!page) {
+      page = 1
+    } else {
+      const schema = Joi.object({
+        page: Joi.number().integer().min(1)
+      })
+      const { error, value } = schema.validate({ page: page })
+      if (error) {
+        return responseStandard(res, error.message, {}, 400, false)
+      }
+      page = value.page
+    }
+
+    const pageInfo = {
+      count: 0,
+      pages: 0,
+      currentPage: page,
+      limitPerPage: limit,
+      nextLink: null,
+      prevLink: null
+    }
 
     const message = await Messages.findAll({
       attributes: {
@@ -176,11 +266,42 @@ module.exports = {
       },
       order: [
         ['createdAt', 'DESC']
-      ]
+      ],
+      limit: limit,
+      offset: (page - 1) * limit
     })
 
     if (message.length) {
-      return responseStandard(res, 'Found message list!', { result: message })
+      const count = await Messages.count({
+        where: {
+          [Op.and]: [
+            {
+              [Op.or]: [
+                {
+                  sender_id: id
+                },
+                {
+                  receiver_id: id
+                }
+              ]
+            },
+            { isLatest: true }
+          ]
+        }
+      })
+      pageInfo.count = count
+      pageInfo.pages = Math.ceil(count / limit)
+      const { pages, currentPage } = pageInfo
+
+      if (currentPage < pages) {
+        pageInfo.nextLink = `${BASE_URL}:${APP_PORT}/message?${qs.stringify({ ...req.query, ...{ page: page + 1 } })}`
+      }
+
+      if (currentPage > 1) {
+        pageInfo.prevLink = `${BASE_URL}:${APP_PORT}/message?${qs.stringify({ ...req.query, ...{ page: page - 1 } })}`
+      }
+
+      return responseStandard(res, 'Found message list!', { pageInfo, result: message })
     } else {
       return responseStandard(res, 'Message list not found!', {}, 404, false)
     }
